0ffb7572e66633bf03b83320ea97012746518edf
Anghello <123@gmail.com>
2025-09-23 00:01:19 -0700

    Complete setting some components and configuration

---
+++ calcify-react/src/pages/Index.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// // import { Calculator } from '../components/calculator/Calculator';
+// // import { CalculatorProvider } from '../contexts/CalculatorContext';
+
+// // const Index = () => {
+// //     return (
+// //         <CalculatorProvider>
+// //          <Calculator />
+// //         </CalculatorProvider> 
+// //     );
+// // };
+
+// export default Index;
