6955107d13cfdf2b90772760e0564ce4e6baf53e
Anghello <123@gmail.com>
2025-09-22 21:16:28 -0700

    Add core functions for calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,19 @@
-print("Welcome to Calcify!")
+import math
+
+def add(n1, n2):
+    """Adds two numbers."""
+    return n1 + n2
+
+def substract(n1, n2):
+    """Subtracts two numbers."""
+    return n1 - n2
+
+def multiply(n1, n2):
+    """Subtracts two numbers."""
+    return n1 * n2
+
+def devide(n1, n2):
+    """Divides two numbers, with a built-in safety check."""
+    if n2 == 0:
+        return "Error: Division by zero is not allowed."
+    return n1 / n2
